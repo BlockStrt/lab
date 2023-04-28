1fe3e361865052bc7f959f91fb7b000c3a80b65b
Blck Strt <blockstarter34@gmail.com>
2023-04-28 12:21:17 -0500

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
       className={'bg-gray-300 h-screen w-full items-center overflow-x-hidden overflow-y-hidden scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60'}
     >
       <Navbar/>
-     
+      <div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
+        hell
+      </div>
     </main>
   )
 }
