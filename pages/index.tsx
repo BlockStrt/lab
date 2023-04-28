fbece24197fb3e9954e63f834c63c270706935cb
Blck Strt <blockstarter34@gmail.com>
2023-04-28 12:15:44 -0500

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex bg-gray-300 h-screen flex-col items-center `}
+      className={'bg-gray-300 h-screen w-full items-center overflow-x-hidden overflow-y-hidden scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60'}
     >
 
         <div className='w-full fixed'><Navbar/></div>
