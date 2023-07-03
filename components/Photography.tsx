9534c09b4f6386d56f96cfc617795e963f625017
Blck Strt <blockstarter34@gmail.com>
2023-07-03 10:19:33 -0500

    update

--- components/Photography.tsx
+++ components/Photography.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { p1, p2, p3, p4 } from '@/clientfirs/images';
 // Import Swiper styles
@@ -13,17 +14,22 @@ const Photography = () => {
     const p4 = 'https://cdn.sanity.io/images/7ezulpzq/production/2a446e8bb6b78b0c0678d90d00939fdad59e2d64-2000x2000.jpg'
 
     return (
-        <section className=' w-screen h-screen pb-10 '>
+        <section className='  pb-10 justify-center items-center mx-auto '>
         <div className=' pb-10 mb-10 justify-center items-center mx-auto'>
         <div className='items-center mx-auto justify-center flex p-3'>swipe here</div>
-            <div className='w-full flex justify-center  items-center mx-auto'>
+            <div className='flex mx-auto w-screen items-center '>
             <Swiper
                 spaceBetween={50}
-                slidesPerView={1}
+                centeredSlides={true}
+                slidesPerView={2}
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
+                pagination={{
+                    clickable: true,
+                  }}
+                  modules={[Pagination]}
                 >
-            <SwiperSlide>
+            <SwiperSlide className=''>
                 <Image src={p1} width={500} height={500} alt='#'/>
             </SwiperSlide>
             <SwiperSlide>
